9720d8f46a8b96c9d0695fbf0025b63d21f37ede
Michael <mduvan.campo@gmail.com>
2024-10-16 06:45:24 -0500

    gh-pages add

--- deploy.sh
+++ deploy.sh
@@ -24,6 +24,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:mich471/fruitlanding.git main:gh-pages
+git push -f git@github.com:mich471/react-frut.git main:gh-pages
 
 cd -
